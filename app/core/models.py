baabce246d933ac6aa5b66b83491572b17ca20ee
Dani <daniaxasabraxas@gmail.com>
2020-11-06 14:53:04 -0300

    Adding data models, including views and urls and working with static files

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    #categoryId  = models.AutoField(primary_key=True)
+    title       = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    #userId      = models.AutoField(primary_key=True)
+    title       = models.CharField(max_length=30)
+    price       = models.IntegerField()
+    description = models.CharField(max_length=60)
+    category    = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def getIndex(request):
+    return render(request, 'base.html')
