b7301a1bfc24a460b6bb39ac79098f8996d04a0f
Dani <daniaxasabaraxas@gmail.com>
2020-11-05 22:51:33 -0300

    Adding Data Models and creating superuser

--- main/core/models.py
+++ main/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.nombre
+
+class Genero(models.Model):
+    nombre = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.nombre
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return self.nombre
+
+class Vinilo(models.Model):
+    nombre = models.CharField(max_length=60)
+    descripcion = models.CharField(max_length=60)
+    precio = models.IntegerField
+    fecha_publicacion = models.DateField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
